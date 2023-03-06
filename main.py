cbe9938aba33215014e23b2d66e3d5ffd971d564
Semen119292210 <108668527+Semen119292210@users.noreply.github.com>
2023-03-06 15:51:07 +0300

    lol

---
+++ main.py
@@ -0,0 +1,6 @@
+if message.text == "Привет":
+    bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+elif message.text == "/help":
+    bot.send_message(message.from_user.id, "Напиши привет")
+else:
+    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
