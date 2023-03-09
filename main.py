0d3de3c322d20cd473475681eff0a9b8430b2dab
Semen119292210 <108668527+Semen119292210@users.noreply.github.com>
2023-03-09 16:49:19 +0300

    lol

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 import telebot
 bot = telebot.TeleBot('6028054306:AAHjhVBKo28gOimJgIetiFOZ18gfz0WVIS0')
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-#@bot.message_handler(content_types=['text', 'document', 'audio'])
 
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-bot.polling(none_stop=True, interval=0)
 name = '';
 surname = '';
 age = 0;
@@ -19,6 +9,7 @@ def start(message):
     if message.text == '/reg':
         bot.send_message(message.from_user.id, "Как тебя зовут?");
         bot.register_next_step_handler(message, get_name); #следующий шаг – функция get_name
+
     else:
         bot.send_message(message.from_user.id, 'Напиши /reg');
 
@@ -26,12 +17,12 @@ def get_name(message): #получаем фамилию
     global name;
     name = message.text;
     bot.send_message(message.from_user.id, 'Какая у тебя фамилия?');
-    bot.register_next_step_handler(message, get_surnme);
+    bot.register_next_step_handler(message, get_surname);
 
 def get_surname(message):
     global surname;
     surname = message.text;
-    bot.send_message('Сколько тебе лет?');
+    bot.send_message(message.from_user.id, 'Сколько тебе лет?');
     bot.register_next_step_handler(message, get_age);
 
 def get_age(message):
@@ -66,3 +57,16 @@ def callback_worker(call):
          bot.send_message(call.message.chat.id, 'Запомню : )');
     elif call.data == "no":
          ...  # переспрашиваем
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+#@bot.message_handler(content_types=['text', 'document', 'audio'])
+
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    elif message.text == "/reg":
+        start('/reg')
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+bot.polling(none_stop=True, interval=0)
